60cddce2520563352fb0e06b5d5b35b5ee64c106
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-11-27 20:07:37 +0000

    trying agenda code

--- App.tsx
+++ App.tsx
@@ -1,30 +1,86 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {Calendar, LocaleConfig, Agenda} from 'react-native-calendars';
+import { addDays, format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
-export default function App() {
-  const [selected, setSelected] = useState("");
+type Item = {
+  name: string;
+  cookies: boolean;
+};
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
+const App: React.FC = () => {
+  const [items, setItems] = useState<{[key: string]: Post[]}>({});
+
+  useEffect(() => {
+    // run once
+
+    const getData = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      const data: Post[] = await response.json();
+
+      const mappedData = data.map((post, index) => {
+        const date = addDays(new Date(), index);
+
+        return {
+          ...post,
+          date: format(date, 'yyyy-MM-dd'),
+        };
+      });
+
+      const reduced = mappedData.reduce(
+        (acc: {[key: string]: Post[]}, currentItem) => {
+          const {date, ...coolItem} = currentItem;
+
+          acc[date] = [coolItem];
+
+          return acc;
+        },
+        {},
+      );
+
+      setItems(reduced);
+    };
+
+    getData();
+  }, []);
+
+  const renderItem = (item: Item) => {
+    return (
+      <View style={styles.itemContainer}>
+        <Text>{item.name}</Text>
+        <Text>{item.cookies ? `🍪` : `😋`}</Text>
+      </View>
+    );
+  };
 
   return (
-    <View style={styles.container}>
-      <Calendar
-        onDayPress={day => {
-          setSelected(day.dateString);
-        }}
-        markedDates={{
-          [selected]: {selected: true, disableTouchEvent: true}
-        }}
-      />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <Agenda items={items} renderItem={renderItem} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    margin: 5,
+    borderRadius: 15,
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
 });
+
+export default App;
