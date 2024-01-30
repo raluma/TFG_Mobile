7dec7ab57f11167c0007a4257d89c03753074850
Raúl Marrero Quevedo <ramaque01@gmail.com>
2024-01-30 19:13:44 +0000

    trimming config

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['module:react-native-dotenv'], 
-      {
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }
+      ['module:react-native-dotenv']
     ]
   };
 };
