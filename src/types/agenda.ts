c18547fdc39f5620b5042b66c044d1360146ac49
Raúl Marrero Quevedo <ramaque01@gmail.com>
2024-02-13 18:11:16 +0000

    creating tipes in a new file

---
+++ src/types/agenda.ts
@@ -0,0 +1,13 @@
+export type Item = {
+    name: string;
+};
+
+export type Post = {
+    id: number;
+    name: string;
+    tag: string;
+    time: string;
+    height: number;
+    day: string;
+    userId: number;
+};
