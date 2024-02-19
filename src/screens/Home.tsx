f5dc6200433b816d99bfcd0dbdbf5ee0dd8480b1
Raúl Marrero Quevedo <ramaque01@gmail.com>
2024-02-19 19:23:01 +0000

    cleaning

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,5 @@
-import { addDays, format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import Card from '../components/Card';
 import ModalAction from '../components/ModalAction';
@@ -15,42 +14,27 @@ const Home = ({ session }) => {
   useEffect(() => {
     // run once
 
-    const getData = () => {
-
-      const data: Post[] = [
-        {
-          id: 1,
-          name: "Cocinar almuerzo",
-          tag: "Cocina",
-          time: "10:00 - 11:00",
-          height: 0,
-          day: "10",
-          userId: 1,
-        },
-        {
-          id: 2,
-          name: "Ir a la cancha",
-          tag: "Deporte",
-          time: "10:00 - 11:00",
-          height: 0,
-          day: "12",
-          userId: 1,
-        }
-      ]
-
-      const mappedData = data.map((post, index) => {
-        // const date = addDays(new Date(), index);
-        const date = "2024-02-18";
-
+    onSnapshot(collection(db, "Events"), (snapshot) => {
+      const mappedData = snapshot.docs.map((doc, index) => {
         return {
-          ...post,
-          date: date,
+          ...{ 
+            id: doc.id, 
+            name: doc.data()["name"], 
+            desc: doc.data()["description"],
+            tag: doc.data()["tag"],
+            time: doc.data()["time"],
+            height: parseInt(doc.data()["height"]), 
+            day: doc.data()["day"], 
+            userId: parseInt(doc.data()["userId"])
+          },
+          date: doc.data()["date"],
         };
-      });
+      })
 
       const reduced = mappedData.reduce(
         (acc: {[key: string]: Post[]}, currentItem) => {
           const {date, ...item} = currentItem;
+
 
           if (Array.isArray(acc[date])) {
             acc[date].push(item);
@@ -64,49 +48,8 @@ const Home = ({ session }) => {
       );
 
       setItems(reduced);
-    };
-
-    // const getData = onSnapshot(collection(db, "Events"), (snapshot) => {
-    //   const mappedData = snapshot.docs.map((doc, index) => {
-
-    //     return {
-    //       ...{ 
-    //         id: parseInt(doc.id), 
-    //         name: doc.data()["name"], 
-    //         tag: doc.data()["tag"],
-    //         time: doc.data()["time"],
-    //         height: parseInt(doc.data()["height"]), 
-    //         day: doc.data()["day"], 
-    //         userId: parseInt(doc.data()["userId"])
-    //       },
-    //       date: doc.data()["date"],
-    //     };
-    //   });
-
-    //   const reduced = mappedData.reduce(
-    //     (acc: {[key: string]: Post[]}, currentItem) => {
-    //       const {date, ...coolItem} = currentItem;
-
-    //       acc[date] = [coolItem];
-
-    //       return acc;
-    //     },
-    //     {},
-    //   );
-
-    //   setItems(reduced);
-    // });
-
-    getData();
+    });
   }, []);
-
-  const renderEmptyDate = () => {
-    return (
-      <View>
-        <Text>This is empty date!</Text>
-      </View>
-    );
-  };
 
   const renderItem = (item: Item) => {
     return (
